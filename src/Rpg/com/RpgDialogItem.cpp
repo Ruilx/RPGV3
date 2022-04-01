fae67e44a487aa49d165d1a09bc0851b15815eea
zhangrui37 <zhangrui37@baidu.com>
2022-04-01 19:05:47 +0800

    Update code

--- src/Rpg/com/RpgDialogItem.cpp
+++ src/Rpg/com/RpgDialogItem.cpp
@@ -6,6 +6,31 @@
 
 #include <Rpg/core/RpgState.h>
 #include <Rpg/core/RpgFont.h>
+#include <Rpg/core/RpgAvatar.h>
+
+void RpgDialogItem::appendMessage(const RpgDialogMessage &message){
+	if(this->isRunning()){
+		rWarning() << "Cannot append message when dialog running.";
+		return;
+	}
+	this->messages.append(message);
+}
+
+void RpgDialogItem::appendMessage(const QList<RpgDialogMessage> &messages){
+	if(this->isRunning()){
+		rWarning() << "Cannot append messages when dialog running.";
+		return;
+	}
+	this->messages.append(messages);
+}
+
+void RpgDialogItem::appendMessage(const QString &text, const QString &name){
+	if(this->isRunning()){
+		rWarning() << "Cannot append message when dialog is running.";
+		return;
+	}
+	this->messages.append(RpgDialogMessage(text, name));
+}
 
 void RpgDialogItem::setLineHeight(qreal pixel, int lineHeightType){
 	QTextCursor textCursor = this->messageBox->textCursor();
@@ -76,4 +101,118 @@ RpgDialogItem::RpgDialogItem(RpgDialogBase *dialogBase, QGraphicsObject *parent)
 
 RpgDialogItem::~RpgDialogItem(){
 	rpgState->unregisterRpgObject(this, RpgState::DialogMode);
+}
+
+void RpgDialogItem::showDialog(){
+	this->show();
+	rpgState->pushState(RpgState::DialogMode);
+	this->showMessage(0);
+}
+
+void RpgDialogItem::showMessage(index){
+	if(index < 0 || index > this->messages.length()){
+		rWarning() << "Index out of range: [0," << this->messages.length() << ")";
+		return;
+	}
+	const RpgDialogMessage current = this->messages.at(index);
+	QString characterName = current.getCharacterName();
+	Rpg::AvatarMode mode = current.getAvatarMode(); // 前半身立绘, 后半身立绘, 头像
+	Rpg::AvatarAround around = current.getAvatarAround(); // 左, 右
+	RpgDialogAnimation::Animations animations = 0;
+	if(index = 0){
+		animations.setFlag(RpgDialogAnimation::AnimationDialogShow);
+	}
+
+	if(!characterName.isEmpty()){
+		// 有立绘/头像
+		// 从 无/头像 -> 立绘 => 立绘头像进入动画
+		// 从 立绘 -> 头像/无 => 立绘头像退出动画
+		// 从 立绘左 -> 立绘右 => 左侧退出, 右侧进入
+		// 从 立绘右 -> 立绘左 => 右侧退出, 左侧进入
+		QPixmap avatarPixmap = rpgAvatar->getAvatarAdjusted(characterName, mode, this->dialogSize - (2 * QSize(MessageMarginH, MessageMarginV)));
+		if(mode == Rpg::AvatarHalfBodyFront || mode == Rpg::AvatarHalfBodyBehind){
+			// 本次半身立绘
+			if(around == Rpg::AvatarAroundLeft){
+				if(around != (*this->lastDialogMessage).getAvatarAround()){
+					// Left, not same as last
+					animations.setFlag(RpgDialogAnimation::AnimationLeftAvatarShow);
+					if((*this->lastDialogMessage).getAvatarAround() == Rpg::AvatarAroundRight){
+						animations.setFlag(RpgDialogAnimation::AnimationRightAvatarHide);
+					}
+					this->avatarBoxLeft->setPixmap(avatarPixmap);
+					this->avatarBoxLeft->setPos(RpgDialogAnimation::leftAvatarOutsidePos(avatarPixmap.size()));
+					this->avatarBoxLeft->setOpacity(0);
+				}else{
+					// Left, only change pixmap
+					this->avatarBoxLeft->setPixmap(avatarPixmap);
+					this->avatarBoxLeft->setPos(RpgDialogAnimation::leftAvatarInsidePos(avatarPixmap.size()));
+				}
+
+				if(mode == Rpg::AvatarHalfBodyFront){
+					qreal x = (qreal)avatarPixmap.width() - this->box->pos().x();
+					if(x > 0){
+						this->messageBox->setPos(x + MessageMarginH, MessageMarginV);
+					}else{
+						this->messageBox->setPos(MessageMarginH, MessageMarginV);
+					}
+					this->setMessageTextWidth(dialogSize.width() - x - MessageMarginH * 2);
+					this->avatarBoxLeft->setZValue(0.1f);
+				}
+
+				if(!this->avatarBoxLeft->isVisible()){
+					this->avatarBoxLeft->show();
+				}
+			}else if(around == Rpg::AvatarAroundRight){
+				if(around != (*this->lastDialogMessage).getAvatarAround()){
+					// Right, not same as last
+					animations.setFlag(RpgDialogAnimation::AnimationRightAvatarShow);
+					if((*this->lastDialogMessage).getAvatarAround() == Rpg::AvatarAroundLeft){
+						animations.setFlag(RpgDialogAnimation::AnimationLeftAvatarHide);
+					}
+					this->avatarBoxRight->setPixmap(avatarPixmap);
+					this->avatarBoxRight->setPos(RpgDialogAnimation::rightAvatarOutsidePos(avatarPixmap.size()));
+					this->avatarBoxRight->setOpacity(0);
+				}else{
+					// Right, only change pixmap
+					this->avatarBoxRight->setPixmap(avatarPixmap);
+					this->avatarBoxRight->setPos(RpgDialogAnimation::rightAvatarInsidePos(avatarPixmap.size()));
+				}
+
+				if(mode == Rpg::AvatarHalfBodyFront){
+					QPointF dialogPos = this->box->pos();
+					this->messageBox->setPos(QPointF(MessageMarginH, MessageMarginV));
+					qreal textWidth = qMin(dialogSize.width() - avatarPixmap.width() + (ScreenWidth - dialogPos.x() - dialogSize.width()), qreal(dialogSize.width()));
+					if(textWidth > 0){
+						this->setMessageTextWidth(textWidth);
+					}else{
+						this->setMessageTextWidth(dialogSize.width() - 2 * MessageMarginH);
+					}
+					this->avatarBoxRight->setZValue(0.1f);
+				}else if(mode == Rpg::AvatarHalfBodyBehind){
+					// Avatar behind
+					this->messageBox->setPos(QPointF(MessageMarginH, MessageMarginV));
+					this->setMessageTextWidth(dialogSize.width() - 2 * MessageMarginH);
+					this->avatarBoxRight->setZValue(-0.1f);
+				}
+
+				if(!this->avatarBoxRight->isVisible()){
+					this->avatarBoxRight->show();
+				}
+			}
+		}else if(mode == Rpg::AvatarFrame){
+			// header avatar
+			if(around == Rpg::AvatarAroundLeft){
+				// Left, need repos
+				if((*this->lastDialogMessage).getAvatarMode() != Rpg::AvatarFrame){
+					// mode not same, force to remove avatars
+					if((*this->lastDialogMessage).getAvatarAround() == Rpg::AvatarAroundLeft){
+						// same position, waiting to avatar exit
+						RpgDialogAnimation::Animations animationsTemp = 0;
+					}
+				}
+			}
+		}
+	}
+
+
 }
--- src/Rpg/com/RpgDialogItem.h
+++ src/Rpg/com/RpgDialogItem.h
@@ -14,81 +14,83 @@
 class RpgDialogItem : public RpgObject
 {
 	Q_OBJECT
-
+	// 皮肤基类
 	RpgDialogBase *skin = nullptr;
 
+	// 构成
 	QGraphicsPixmapItem *box = new QGraphicsPixmapItem(this);
 	QGraphicsTextItem *messageBox = new QGraphicsTextItem(this->box);
 	QGraphicsPixmapItem *continueSymbol = new QGraphicsPixmapItem(this->box);
-
+	// 立绘
 	QGraphicsPixmapItem *avatarBoxRight = new QGraphicsPixmapItem(this);
 	QGraphicsPixmapItem *avatarBoxLeft = new QGraphicsPixmapItem(this);
-
+	// 效果
 	QGraphicsDropShadowEffect *messageShadowEffect = new QGraphicsDropShadowEffect(this);
-
+	// 动画组成
 	RpgDialogAnimation *dialogAnimation = new RpgDialogAnimation(this->box, this->avatarBoxLeft, this->avatarBoxRight, 300, QEasingCurve::OutQuad, this);
-
+	// 对话框大小
 	QSize dialogSize = RpgDialogBase::dialogSize();
-
+	// 对话框对齐
 	Rpg::BlockAlign dialogAlign = Rpg::AlignBottom;
 
-	void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
-	void boundingRect();
+	// 继承函数
+//	void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+//	void boundingRect() override;
 
+	// 按键
 	void keyReleaseEvent(QKeyEvent *event) override;
 public:
+	// 消息框内部间距
 	const int MessageMarginH = 10;
 	const int MessageMarginV = 5;
 
+	// 消息使用内部字体
 	const char *FontName = "dialog";
 private:
+
+	// 消息列表
 	QList<RpgDialogMessage> messages;
 	int messageIndex = 0;
 	QList<RpgDialogMessage>::ConstIterator lastDialogMessage;
 
+	// 正在运行的Flag
 	bool showTextInProgressFlag = false;
 	bool quickShowFlag = false;
 
+	// 继续三角形 时间轴
 	QTimeLine *continueSymbolTimeLine = new QTimeLine(1000, this);
+
 
 	inline void setMessageTextWidth(qreal width){
 		this->messageBox->setTextWidth(width);
 	}
 
 public:
-	inline void setTextColor(const QColor &color){
-		this->messageBox->setDefaultTextColor(color);
-	}
+	// 文字颜色(不含CSS标签颜色)
+	inline void setTextColor(const QColor &color){ this->messageBox->setDefaultTextColor(color); }
+	inline void setTextColor(Qt::GlobalColor color){ this->messageBox->setDefaultTextColor(QColor(color)); }
 
-	inline void setTextColor(Qt::GlobalColor color){
-		this->messageBox->setDefaultTextColor(QColor(color));
-	}
+	// 增加消息
+	void appendMessage(const RpgDialogMessage &message);
+	void appendMessage(const QList<RpgDialogMessage> &messages);
+	void appendMessage(const QString &text, const QString &name = QString());
 
-	inline void appendMessage(const RpgDialogMessage &message){
-		this->messages.append(message);
-	}
+	// 字体
+	inline void setFont(const QFont &font){ this->messageBox->setFont(font); }
+	inline QFont getFont() const{ return this->messageBox->font(); }
 
-	inline void appendMessage(const QList<RpgDialogMessage> &messages){
-		this->messages.append(messages);
-	}
-
-	inline void appendMessage(const QString &text, const QString &name = QString()){
-		this->messages.append(RpgDialogMessage(text, name));
-	}
-
-	inline void setFont(const QFont &font){
-		this->messageBox->setFont(font);
-	}
-
-	inline QFont getFont() const{
-		return this->messageBox->font();
-	}
-
+	// 行高
 	void setLineHeight(qreal pixel, int lineHeightType = QTextBlockFormat::FixedHeight);
 
 
 	RpgDialogItem(RpgDialogBase *dialogBase, QGraphicsObject *parent = nullptr);
 	~RpgDialogItem();
+
+private:
+	void showDialog();
+	void hideDialog();
+
+	void showMessage(index);
 };
 
 #endif // RPGDIALOGITEM_H
